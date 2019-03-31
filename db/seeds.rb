fa577e03e19f6488c8ff63c5af7ece01cb9ed726
debjohnson33 <debjohnson_11@yahoo.com>
2019-03-30 20:22:48 -0500

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Monster.destroy_all
+
+ari = User.create!(username: 'ari', email: 'ari@ga.co', password: 'passwrod')
+j = User.create!(username: 'j', email: 'jsx@ga.co', password: 'password')
+bell = User.create!(username: 'bell', email: 'bell@ga.co', password: 'password')
+dom = User.create!(username: 'dom', email: 'dom@ga.co', password: 'password')
+ramsey = User.create!(username: 'ramsey', email: 'ramsey@ga.co', password: 'password')
+drew = User.create!(username: 'drew', email: 'drew@ga.co', password: 'password')
+
+puts "#{User.count} users created!"
+
+Monster.create!(user: ari, name: 'Stacey', description: 'A friendly monster')
+Monster.create!(user: j, name: 'Tracey', description: 'A nice monster')
+Monster.create!(user: bell, name: 'Macey', description: 'A silly monster')
+Monster.create!(user: dom, name: 'Lacey', description: 'A goofy monster')
+Monster.create!(user: ramsey, name: 'Kacey', description: 'A wonderful monster')
+Monster.create!(user: drew, name: 'Jacey', description: 'An excellent monster')
+
+puts "#{Monster.count} monsters created!"
