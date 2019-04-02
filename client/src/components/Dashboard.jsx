218d1dd1e4be3db14af7b00e2df2eaba173d2a5a
debjohnson33 <debjohnson_11@yahoo.com>
2019-04-01 19:24:37 -0500

    Added add monster functionality

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Auth from '../modules/Auth';
+import AddMonsterForm from './AddMonsterForm';
 
 class Dashboard extends Component {
     constructor() {
@@ -11,6 +12,10 @@ class Dashboard extends Component {
     }
 
     componentDidMount() {
+        this.getUserMonsters();
+    }
+
+    getUserMonsters() {
         fetch('/profile', {
             method: 'GET',
             headers: {
@@ -26,9 +31,28 @@ class Dashboard extends Component {
           }).catch(err => console.log(err));
     }
 
+    addMonster(e, data) {
+        fetch('/monsters', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                token: Auth.getToken(),
+                'Authorization' : `Token ${Auth.getToken()}`
+            },
+            body: JSON.stringify({
+                monster: data
+            })
+        }).then(res => res.json())
+          .then(res => {
+              console.log(res);
+              this.getUserMonsters();
+          }).catch(err => console.log(err));
+    }
+
     render() {
         return (
             <div className="dash">
+                <AddMonsterForm addMonster={this.addMonster} />
                 {(this.state.monstersLoaded)
                   ? this.state.myMonsters.map (monster => {
                       return <h1 key={monster.id}>{monster.name}</h1>
